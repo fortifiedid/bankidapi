4851a391aa261287b1fb697b64f91a2db93b693e
Anders Andrén <aandren@gmail.com>
2022-05-16 09:50:53 +0200

    no message

--- version_wizard.sh
+++ version_wizard.sh
@@ -1,76 +1,84 @@
 #!/bin/sh
 
-set -e
+revision=$1
+version=$(echo "$revision" | cut -f1 -d'-')
 
-MASTER="main"
-RELEASE="release"
-MAVEN_CONFIG=".mvn/maven.config"
-LOCAL_RELEASE=
-RELEASE_CMD="mvn clean deploy"
-
-awsLogin() {
-  aws ecr get-login-password --region eu-central-1 | docker login --username "$AWS_USERNAME" --password-stdin "$AWS_ACCOUNT"
-}
-
-releaseWithTag() {
-  git add $MAVEN_CONFIG
-  git commit -m "Preparing release $1"
-  git tag "$1"
-  if [ -z "$LOCAL_RELEASE" ]; then
-    git push
-    git push origin "$1"
-  fi
-  git checkout "$1"
-}
-
-mergeChanges() {
-  printf '%s' "Merge $MASTER into $RELEASE, OK? [yN]"
-  read -r yn
-  case $yn in
-  'y')
-    git merge "$MASTER"
-    ;;
-  *)
-    printf '%s\n' "Merge skipped"
-    ;;
-  esac
-}
-
-init() {
-  git checkout $MASTER
-  if [ -z "$LOCAL_RELEASE" ]; then
-    git pull
-  fi
-  git checkout $RELEASE
-  if [ -z "$LOCAL_RELEASE" ]; then
-    git pull
-  fi
-}
-
-if [ -z "$LOCAL_RELEASE" ]; then
-  printf '%s\n' "Not a local release, will try to apply changes to remote repositories"
-else
-  printf '%s\n' "Local release, no changes will be applied to remote repositories"
+isReleaseCandidate=$(echo "$revision" | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+.*\-RC[0-9]\+$")
+isRelease=$(echo "$revision" | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$")
+if [ -z "$isReleaseCandidate" ] && [ -z "$isRelease" ]; then
+  printf '%s\n' "Invalid revision $revision, cannot infer next version number" >/dev/tty
+  printf '%s' "Enter custom version: " >/dev/tty
+  read -r selectedVersion </dev/tty
+  printf '\n'
 fi
-init
-currentRevision=$(cut -f1- -w "$MAVEN_CONFIG" | tr -s "[:space:]" "\n" | grep "\-Drevision=" | cut -c12-)
-userResponse=$(./version_wizard.sh $currentRevision)
-set -- $userResponse
-nextRevision=$1
-if [ -z $2 ]; then
-  mergeChanges
+
+if [ -n "$isReleaseCandidate" ] && [ -z "$selectedVersion" ]; then
+  currentRc=$(echo "$revision" | cut -f2 -d'-' | cut -c3-)
+  nextRc="$version-RC$((currentRc + 1))"
+  printf '%s\n' "You are currently on Release Candidate $revision. What would you like to do?" >/dev/tty
+  printf '%s\n' " [1] Build next RC $nextRc in this release cycle" >/dev/tty
+  printf '%s\n' " [2] Build release $version from current RC " >/dev/tty
+  printf '%s\n' " [3] Begin new release cycle" >/dev/tty
+  while true; do
+    read -r response </dev/tty
+    case $response in
+    '1')
+      selectedVersion="$nextRc"
+      break
+      ;;
+    '2')
+      selectedVersion="$version 1"
+      break
+      ;;
+    '3')
+      selectedVersion=
+      break
+      ;;
+    *)
+      printf '%s\n' "Unknown response, try again:" >/dev/tty
+      ;;
+    esac
+  done
 fi
-printf '%s' "Are you ready to release $nextRevision?"
-read -r yn
-case $yn in
-'y')
-  printf '%s\n' "Executing release..."
-  sed -i.bak 's/Drevision='"$currentRevision"'/Drevision='"$nextRevision"'/' "$MAVEN_CONFIG"
-  releaseWithTag "v$nextRevision"
-  /bin/sh -c "$RELEASE_CMD"
-  ;;
-*)
-  printf '%s\n' "Release cancelled"
-  ;;
-esac
-printf '%s\n' "Done"
+
+if [ -z "$selectedVersion" ]; then
+  major=$(echo "$revision" | cut -f1 -d'.')
+  minor=$(echo "$revision" | cut -f2 -d'.')
+  patch=$(echo "$revision" | cut -f3 -d'.' | cut -f1 -d'-')
+  nextMajor="$((major + 1)).0.0-RC1"
+  nextMinor="$major.$((minor + 1)).0-RC1"
+  nextPatch="$major.$minor.$((patch + 1))-RC1"
+  printf '%s\n' "You are currently on revision: $revision. What would you like to do?" >/dev/tty
+  printf '%s\n' " [1] Begin new patch release cycle ($nextPatch)?" >/dev/tty
+  printf '%s\n' " [2] Begin new minor release cycle ($nextMinor)?" >/dev/tty
+  printf '%s\n' " [3] Begin new major release cycle ($nextMajor)?" >/dev/tty
+  printf '%s\n' " [4] Build new release with custom version" >/dev/tty
+  while true; do
+    read -r response </dev/tty
+    case $response in
+    '1')
+      selectedVersion="$nextPatch"
+      break
+      ;;
+    '2')
+      selectedVersion="$nextMinor"
+      break
+      ;;
+    '3')
+      selectedVersion="$nextMajor"
+      break
+      ;;
+    '4')
+      printf '%s' "Enter custom version: " >/dev/tty
+      read -r selectedVersion </dev/tty
+      printf '\n'
+      break
+      ;;
+    *)
+      printf '%s\n' "Unknown response, try again:" >/dev/tty
+      ;;
+    esac
+  done
+fi
+
+echo "$selectedVersion"
